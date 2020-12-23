7a5388126e661578c5a2c9e53be62ec01bb2af95
huskpurin <huskpurin@gmail.com>
2020-12-23 23:41:25 +0700

    update scripts for static pages build and deployment

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+const proj = require('./package.json').name;
+
+module.exports = {
+  pathPrefix: `/${proj}`,
+  plugins: [
+    {
+      resolve: 'gatsby-theme-mdx-deck',
+      options: {
+        cli: true,
+        contentPath: 'src/slides',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-compile-es6-packages',
+      options: {
+        modules: ['mdx-deck', 'gatsby-theme-mdx-deck', '@mdx-deck/themes', '@mdx-deck'],
+      },
+    },
+  ],
+};
